56b25989546a738b35ddbbc5bc7b5cb9a2e8d7c6
danielld75 <danielld@poczta.fm>
2018-04-06 22:31:49 +0200

    add styles for links

--- client/modules/App/components/Navi/Navi.js
+++ client/modules/App/components/Navi/Navi.js
@@ -1,12 +1,13 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import styles from './navi.css';
 
 export function Navi(props, context) {
   return (
     <span>
-        <li><Link to={'/home'}>Home</Link></li>
-        <li><Link to={'/about'}>About</Link></li>
-        <li><Link to={'/'}>Posts</Link></li>
+        <li><Link to={'/home'} activeClassName={styles['active']}>Home</Link></li>
+        <li><Link to={'/about'} activeClassName={styles['active']}>About</Link></li>
+        <li><Link to={'/'} activeClassName={styles['active']} onlyActiveOnIndex>Posts</Link></li>
     </span>
   );
 }
